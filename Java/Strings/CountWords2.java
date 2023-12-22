28db9e294dec8e35fdd7e3fbd332909219b32338
pranulkbv28 <28bvpranaav@gmail.com>
2023-12-22 21:24:43 +0530

    new files

--- Java/Strings/CountWords2.java
+++ Java/Strings/CountWords2.java
@@ -3,7 +3,7 @@ package Strings;
 import java.util.Scanner;
 
 class CountWords2 {
-    public static void countWords(String str){
+    public static void countWords(String str) {
         String s[] = str.split(" ");
         System.out.println("The total number of words in the Sentence is: "+s.length);
     }
