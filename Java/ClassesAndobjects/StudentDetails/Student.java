4f47c7ff7653f69fcd197286b3af7d94dba361bf
pranulkbv28 <28bvpranaav@gmail.com>
2024-01-11 19:54:34 +0530

    new files added

--- Java/ClassesAndobjects/StudentDetails/Student.java
+++ Java/ClassesAndobjects/StudentDetails/Student.java
@@ -10,8 +10,8 @@ public class Student {
 
     
     
-    public void printDOB(){
-        System.out.println(dob);
+    public String printDOB(){
+        return dob;
     }
     public static void main(String[] args) {
         Student student = new Student();
