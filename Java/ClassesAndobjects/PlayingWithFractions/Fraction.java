9171c30f37fe36f7cd306777293ada7cff0be0b6
pranulkbv28 <28bvpranaav@gmail.com>
2024-01-13 22:28:53 +0530

    new files added

--- Java/ClassesAndobjects/PlayingWithFractions/Fraction.java
+++ Java/ClassesAndobjects/PlayingWithFractions/Fraction.java
@@ -4,7 +4,29 @@ public class Fraction {
     private int numerator;
     private int denominator;
 
+    
+    public void setNumerator(int numerator){
+        this.numerator=numerator;
+        simplify();
+    }
+    public void setDenominator(int num){
+        if(num==0){
+            return;
+        }
+        this.denominator=num;
+        simplify();
+    }
+    public int getNumerator(){
+        return numerator;
+    }
+    public int getDenominator(){
+        return denominator;
+    }
+
     public Fraction(int numerator, int denominator){
+        if(denominator==0){
+            denominator=1;
+        }
         this.numerator = numerator;
         this.denominator = denominator;
         simplify();
@@ -26,5 +48,19 @@ public class Fraction {
     }
     public void print(){
         System.out.println(numerator+"/"+denominator);
+    }
+    public void add(Fraction f2){
+        // if(denominator1!=denominator2){
+        //     numerator1*=denominator2;
+        //     denominator1*=denominator2;
+        //     numerator2*=denominator1;
+        //     denominator2*=denominator1;
+        // }
+        // numerator=numerator1+numerator2;
+        // denominator=denominator1;
+        // simplify();
+        this.numerator = (this.numerator*f2.denominator)+(f2.numerator*this.denominator);
+        this.denominator=this.denominator*f2.denominator;
+        simplify();
     }
 }
--- Java/ClassesAndobjects/PlayingWithFractions/GetFraction.java
+++ Java/ClassesAndobjects/PlayingWithFractions/GetFraction.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class GetFraction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numerator = sc.nextInt();
-        int denominator = sc.nextInt();
-        Fraction f1 = new Fraction(numerator,denominator);
+        int numerator1 = sc.nextInt();
+        int denominator1 = sc.nextInt();
+        Fraction f1 = new Fraction(numerator1, denominator1);
+        int numerator2 = sc.nextInt();
+        int denominator2 = sc.nextInt();
+        Fraction f2 = new Fraction(numerator2, denominator2);
         f1.print();
-        f1.increment();
+        f2.print();
+        // f1.increment();
+        // f1.print();
+        f1.add(f2);
         f1.print();
-
+        
         sc.close();
     }
 }
