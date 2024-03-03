a387b9c3579b9f214fc72874c1d57c575018a0ad
pranulkbv28 <28bvpranaav@gmail.com>
2024-03-03 23:58:57 +0530

    cpp files

--- C++/IntroToC++.cpp
+++ C++/IntroToC++.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 // macro definition
 #define LIMIT 5
+#define AREA(l,b) (l*b)
+
+// int main() {
+//     for (int i = 0; i < LIMIT; i++) {
+//         cout << i << " "<<endl;
+//     }
+//     return 0;
+// }
 
 int main() {
-    for (int i = 0; i < LIMIT; i++) {
-        cout << i << " "<<endl;
-    }
+    int l = 10, b = 5, a;
+    a = AREA(l, b);
+    cout << "The Area of the rectangle is: " << a;
     return 0;
 }
