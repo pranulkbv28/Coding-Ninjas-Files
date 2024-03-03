86d292edc47a8c4cc51acb18b08a9688d820e9ff
pranulkbv28 <28bvpranaav@gmail.com>
2024-03-03 23:48:34 +0530

    files changed

---
+++ C++/IntroToC++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// macro definition
+#define LIMIT 5
+
+int main() {
+    for (int i = 0; i < LIMIT; i++) {
+        cout << i << " "<<endl;
+    }
+    return 0;
+}
