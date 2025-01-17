20c06d65291a837518387827fc46e318e088cfc0
pranulkbv28 <28bvpranaav@gmail.com>
2025-01-17 21:44:27 +0530

    Refactor HTML structure and improve JavaScript theme toggle functionality in Music Player

--- Major Projects/JS/Music Player/script.js
+++ Major Projects/JS/Music Player/script.js
@@ -6,33 +6,33 @@ const themeSection = document.getElementById("themeSection");
 const themeToggle = document.getElementById("themeToggle");
 const themeSlider = document.getElementById("slider");
 themeSlider.addEventListener("click", () => {
-    if (themeSlider.classList.contains("slider-left")) {
-        body.classList.remove("body-light");
-        body.classList.add("body-dark");
-        allNodes.forEach(node => {
-            if (node.classList) {
-                node.classList.add("text-dark");
-            }
-        })
-        themeSlider.classList.remove("slider-left");
-        themeSlider.classList.add("slider-right");
-        themeToggle.classList.remove("theme-toggle-light");
-        themeToggle.classList.add("theme-toggle-dark");
-    } else {
-        body.classList.remove("body-dark");
-        body.classList.add("body-light");
-        allNodes.forEach(node => {
-            if (node.classList) {
-                node.classList.remove("text-dark");
-            }
-        })
-        themeSlider.classList.remove("slider-right");
-        themeSlider.classList.add("slider-left");
-        themeToggle.classList.remove("theme-toggle-dark");
-        themeToggle.classList.add("theme-toggle-light");
-        document.body.style.backgroundColor = rgb(51, 51, 51);
-    }
-})
+  if (themeSlider.classList.contains("slider-left")) {
+    body.classList.remove("body-light");
+    body.classList.add("body-dark");
+    allNodes.forEach((node) => {
+      if (node.classList) {
+        node.classList.add("text-dark");
+      }
+    });
+    themeSlider.classList.remove("slider-left");
+    themeSlider.classList.add("slider-right");
+    themeToggle.classList.remove("theme-toggle-light");
+    themeToggle.classList.add("theme-toggle-dark");
+  } else {
+    body.classList.remove("body-dark");
+    body.classList.add("body-light");
+    allNodes.forEach((node) => {
+      if (node.classList) {
+        node.classList.remove("text-dark");
+      }
+    });
+    themeSlider.classList.remove("slider-right");
+    themeSlider.classList.add("slider-left");
+    themeToggle.classList.remove("theme-toggle-dark");
+    themeToggle.classList.add("theme-toggle-light");
+    document.body.style.backgroundColor = rgb(51, 51, 51);
+  }
+});
 
 const bodySection = document.getElementById("bodySection");
 const allSongsSection = document.getElementById("allSongsSection");
@@ -41,7 +41,7 @@ const playlistSection = document.getElementById("playlistSection");
 const footerSection = document.getElementById("footerSection");
 
 const currDate = new Date();
-const year = currDate.getFullYear()
+const year = currDate.getFullYear();
 footerSection.innerHTML = `
     <span>
         Made with ❤️ by pranulkbv28
@@ -49,4 +49,4 @@ footerSection.innerHTML = `
     <span>
         © ${year}
     </span>
-`
+`;
