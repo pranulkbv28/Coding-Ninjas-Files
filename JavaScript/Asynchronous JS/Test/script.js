d36b795b54ccc8f0510ac4bde8f2e1d7329301e0
pranulkbv28 <28bvpranaav@gmail.com>
2025-01-20 16:05:29 +0530

    Add initial implementation of Asynchronous JavaScript with example functions and HTML structure v2

--- JavaScript/Asynchronous JS/Test/script.js
+++ JavaScript/Asynchronous JS/Test/script.js
@@ -10,13 +10,13 @@
 //   });
 // });
 
-for (var i = 0; i < 5; i++) {
-  setTimeout(() => {
-    console.log(i);
-  }, 1000);
-}
-for (let i = 0; i < 5; i++) {
-  setTimeout(() => {
-    console.log(i);
-  }, 1000);
-}
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
